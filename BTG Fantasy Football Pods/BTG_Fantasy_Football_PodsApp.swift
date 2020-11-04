4de18963bb27e251eeb69cda7317ff6c10ffb82a
Jon Sweeney <jonsweeney26@gmil.com>
2020-11-03 18:02:20 -0800

    Initial Commit

---
+++ BTG Fantasy Football Pods/BTG_Fantasy_Football_PodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTG_Fantasy_Football_PodsApp.swift
+//  BTG Fantasy Football Pods
+//
+//  Created by Jonathan Sweeney on 11/3/20.
+//
+
+import SwiftUI
+
+@main
+struct BTG_Fantasy_Football_PodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
